0374046be3e7b5575af42b4e19dd3aac8fe24232
Lê Chí Dũng <dung.lc@sis.hust.edu.vn>
2024-05-08 11:42:40 +0700

    commit before merge

--- src/main/java/itss/group22/bookexchangeeasy/entity/ChatMessage.java
+++ src/main/java/itss/group22/bookexchangeeasy/entity/ChatMessage.java
@@ -1,12 +1,10 @@
 package itss.group22.bookexchangeeasy.entity;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity
--- src/main/java/itss/group22/bookexchangeeasy/repository/StoreEventRepository.java
+++ src/main/java/itss/group22/bookexchangeeasy/repository/StoreEventRepository.java
@@ -1,5 +1,4 @@
 package itss.group22.bookexchangeeasy.repository;
-
 import itss.group22.bookexchangeeasy.entity.StoreEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
